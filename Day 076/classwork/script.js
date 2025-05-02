47c2ea8465aa50dc33030ccbb0790bd73ecdc3e8
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2025-05-02 21:00:34 +0400

    commit

---
+++ Day 076/classwork/script.js
@@ -0,0 +1,16 @@
+//1)
+let paragraph = document.getElementsByTagName("p")
+
+paragraph[0].innerHTML = "giorgi"
+paragraph[0].style.color = "red"
+
+paragraph[1].innerHTML = "luka"
+paragraph[1].style.color = "blue"
+
+//2)
+let h1 = document.getElementsByTagName("h1")
+
+function change(){
+    h1[0].innerHTML = "hello gio"
+
+}
