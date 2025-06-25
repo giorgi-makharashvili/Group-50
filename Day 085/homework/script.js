205324ad5b0945b2bed9982de14e02e389386974
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2025-06-25 16:39:40 +0400

    commit

--- Day 085/homework/script.js
+++ Day 085/homework/script.js
@@ -8,67 +8,67 @@ let audios = [
     "./audio/tom-4.mp3",
 ]
 
-let instruments = document.querySelectorAll("a1");
+let instruments = document.querySelectorAll(".a1");
 
 document.addEventListener("keydown", function(event){
     if (event.key === "w"){
         let gaudio = new Audio(audios[0])
         gaudio.play()
-        instruments[0].Style.border = "10px solid #53607c"
+        instruments[0].style.border = "10px solid rgb(128, 130, 136)"
     }
     if (event.key === "a"){
         let gaudio = new Audio(audios[1])
         gaudio.play()
-        instruments[1].Style.border = "10px solid #53607c"
+        instruments[1].style.border = "10px solid rgb(128, 130, 136)"
     }
     if (event.key === "s"){
         let gaudio = new Audio(audios[2])
         gaudio.play()
-        instruments[2].Style.border = "10px solid #53607c"
+        instruments[2].style.border = "10px solid rgb(128, 130, 136)"
     }
     if (event.key === "d"){
         let gaudio = new Audio(audios[3])
         gaudio.play()
-        instruments[3].Style.border = "10px solid #53607c"
+        instruments[3].style.border = "10px solid rgb(128, 130, 136)"
     }
     if (event.key === "j"){
         let gaudio = new Audio(audios[4])
         gaudio.play()
-        instruments[4].Style.border = "10px solid #53607c"
+        instruments[4].style.border = "10px solid rgb(128, 130, 136)"
     }
     if (event.key === "k"){
         let gaudio = new Audio(audios[5])
         gaudio.play()
-        instruments[5].Style.border = "10px solid #53607c"
+        instruments[5].style.border = "10px solid rgb(128, 130, 136)"
     }
     if (event.key === "l"){
         let gaudio = new Audio(audios[6])
         gaudio.play()
-        instruments[6].Style.border = "10px solid #53607c"
+        instruments[6].style.border = "10px solid rgb(128, 130, 136)"
     }
 })
 
 document.addEventListener("keyup", function(event){
     if (event.key === "w") {
-        instruments[0].Style.border = "10px solid #3d465d"
+        instruments[0].style.border = "10px solid  #3d465d"
     }
     if (event.key === "a") {
-        instruments[1].Style.border = "10px solid #3d465d"
+        instruments[1].style.border = "10px solid #3d465d"
     }
     if (event.key === "s") {
-        instruments[2].Style.border = "10px solid #3d465d"
+        instruments[2].style.border = "10px solid #3d465d"
     }
     if (event.key === "d") {
-        instruments[3].Style.border = "10px solid #3d465d"
+        instruments[3].style.border = "10px solid #3d465d"
     }
     if (event.key === "j") {
-        instruments[4].Style.border = "10px solid #3d465d"
+        instruments[4].style.border = "10px solid #3d465d"
     }
     if (event.key === "k") {
-        instruments[5].Style.border = "10px solid #3d465d"
+        instruments[5].style.border = "10px solid #3d465d"
     }
     if (event.key === "l") {
-        instruments[6].Style.border = "10px solid #3d465d"
+        instruments[6].style.border = "10px solid #3d465d"
     }
 })
 
